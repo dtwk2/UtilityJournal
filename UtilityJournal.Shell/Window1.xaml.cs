ee448f8debc675eb3f69a0f9d7531a581aa782a9
Declan Taylor <declan.taylor@pcs-instruments.com>
2022-03-14 10:34:33 +0000

    Add project files.

---
+++ UtilityJournal.Shell/Window1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using DateWork.Models;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace UtilityJournal.Shell
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        DateModel dateModel = new();
+        public Window1()
+        {
+            InitializeComponent();
+            DatePickerControl.Month = dateModel.Month;
+            DatePickerControl.Year = dateModel.Year;
+            MonthGridControl.ItemsSource = dateModel.Days;
+        }
+
+        private void DatePickerControl_DateChange(object sender, UtilityJournal.Controls.DateChangeEventArgs e)
+        {
+            dateModel.Month = e.Month;
+            dateModel.Year = e.Year;
+            MonthGridControl.ItemsSource = dateModel.Days;
+            MonthGridControl.SelectedItem = dateModel.Current;
+        }
+
+        private void MonthGridControl_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            ContentControl.Content = MonthGridControl.SelectedItem;
+        }
+    }
+}
